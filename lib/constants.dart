2eaec78236ae18978835671e4bcf97c9253d8df2
muhalimashar21 <muhalimashar21@gmail.com>
2024-10-05 12:14:02 +0700

    first-push

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(0xFF535353);
+const kTextLightColor = Color(0xFFACACAC);
+const kTextGray = Color(0xFF787878);
+const baseUrl = 'https://api.connectis.my.id/';
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:my_solonet_app/auth/login.dart';
+import 'package:my_solonet_app/auth/register.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SignInScreen(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            print('Login button pressed');
+          },
+          child: Text('Login'),
+        ),
+      ),
+    );
+  }
+}
